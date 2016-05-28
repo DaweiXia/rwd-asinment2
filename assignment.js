56f091680f0cb52ed739965628e20031b240f8ae
Dawei Xia <xiadw_public@163.com>
2016-05-28 15:04:13 +0800

    Add event that can open modal when click button of each animal

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
 Handlebars.registerPartial('animalInfo',
 	'<div class="row">{{#each animals}}<div class="col-xs-6 col-md-3"><div class="thumbnail">'
 	+'<a href="#" data-toggle="modal" data-target="#{{animalName name}}"><img src="{{image1}}" alt="A {{name}}" /></a>'
-	+'<h4>{{name}}</h4></div></div>'
+	+'<h4><a href="#" class="btn btn-default" data-toggle="modal" data-target="#{{animalName name}}">{{name}}</a></h4></div></div>'
 	+'<div class="modal" id="{{animalName name}}" tabindex="-1" role="dialog"><div class="modal-dialog">'
 	+'<div class="modal-content"><div class="modal-header">'
 	+'<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
